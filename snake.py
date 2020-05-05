ad698fcbfeb71232e77bdacf810f8ae4da1aa867
Henry Gabryjelski <henrygab@users.noreply.github.com>
2020-05-05 09:40:32 -0700

    Step 2: Create Group for the display to focus on

--- snake.py
+++ snake.py
@@ -4,3 +4,8 @@ from adafruit_display_shapes.circle import Circle
 import displayio
 import time
 import random
+
+# make a group and make sure that the CLUE's
+# display is focused on this group
+stuff_on_screen = displayio.Group(max_size=100)
+board.DISPLAY.show(stuff_on_screen)
